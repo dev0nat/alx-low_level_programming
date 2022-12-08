701bcce95fe29705fc1f2b7e19cfc38b1d5a9923
dev0nat <natdev01@tuta.io>
2022-12-08 12:00:53 -0800

    prints with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /* main function prints a text*/
-int main()
+int main(void)
 {
 
-puts("Programming is like building a multilingual puzzle \n");
+puts("\"Programming is like building a multilingual puzzle \n");
 
-return 0;
+return (0);
 
 }
 
