5f427fbdbf65afab3356e1c5dec7d7689553f948
dev0nat <natdev01@tuta.io>
2022-12-08 12:26:43 -0800

    prints a string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints out a text
+ * 
+ * Return: returns 0 (sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
+	return(0);
+}
