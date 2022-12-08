27994a61cf38d90d43942fdc3e25fc4699087ac3
dev0nat <natdev01@tuta.io>
2022-12-08 12:33:43 -0800

    prints a string

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,10 @@
 
 /**
  * main - prints out a text
- * 
  * Return: returns 0 (sucess)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
