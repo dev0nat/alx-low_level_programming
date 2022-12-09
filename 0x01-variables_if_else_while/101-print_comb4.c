96a0d1c448dbed45893423382434512b3dab6a09
dev0nat <natdev01@tuta.io>
2022-12-09 14:32:57 -0800

    possible combos of two digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,38 +5,27 @@
  *
  * Return: (0) Success.
  */
-
 int main(void)
 {
 	int x, y, z;
 
-	for (x = 48; x < 58; x++)
+	for (x = '0'; x <= '9'; x++)
 	{
-		for (y = 48; y < 58; y++)
+		for (y = x + 1; y <= '9'; y++)
 		{
-			for (z = 48; z < 58; z++)
+			for (z = y + 1; z <= '9'; z++)
 			{
-				if (x == y || y == z || x == z)
-				{
-					continue;
-				}
 				putchar(x);
 				putchar(y);
 				putchar(z);
-
-				if (x == 55 && y == 56 && z == 57)
-				{
-					break;
-
-				}
-				else
+				if (x != '7' || y != '8' || z != '9')
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
 		}
-		putchar('\n');
 	}
+	putchar('\n');
 	return (0);
 }
