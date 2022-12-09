aa4e036971679010658d4a58596203046383e39d
dev0nat <natdev01@tuta.io>
2022-12-09 13:22:20 -0800

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *  main - prints out hexadecimal values
+ *
+ *  Return: returns 0 (Success)
+ */
+int main(void)
+{
+	int x = 48;
+	char y = 'a'
+
+	for (; x <= 58; x++)
+	putchar(x);
+	for (; y <= 'f'; y++)
+	putchar(y);
+	putchar('\n');
+
+	return (0);
+}
+
