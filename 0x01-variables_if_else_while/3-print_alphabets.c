fcbfecd5de2a6a844f1140addab2ac8caa55dbb3
dev0nat <natdev01@tuta.io>
2022-12-09 12:05:30 -0800

    more alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  main - prints lower & Uppercase a - z
+ *
+ *  Return: returns 0 (Success)
+ */
+int main(void)
+{
+	char x = 'a', 'A';
+
+	for (; x <= 'z'; x++)
+	putchar(x);
+	putchar('\n');
+	for (; y <= 'Z'; y++)
+	putchar(x);
+	putchar('\n');
+
+	return (0);
+}
