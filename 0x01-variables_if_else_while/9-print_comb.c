81856a41f9261d870c7eccbbd6abfa596d3a7026
dev0nat <natdev01@tuta.io>
2022-12-09 14:01:00 -0800

    fixes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,12 @@ int main(void)
 	for (x = '0'; x <= '9'; x++)
 	{
 	putchar(x);
-
-	if (x != '9')
+	if (x == '9')
+		continue;
 	putchar(',');
 	putchar(' ');
 	}
-	/* putchar('\n'); */
+	putchar('\n');
 
 	return (0);
 }
