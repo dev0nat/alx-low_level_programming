e4aa709ad80d00c16ab0cc4d4da360a75fd812ae
dev0nat <natdev01@tuta.io>
2022-12-09 11:08:31 -0800

    minor fixes

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,17 +15,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	n %= 10;
+	printf("%s %i is", str, n)
 	if (n > 5)
 	{
-	printf("%s %i is and is greater than 5\n", str, n);
+	printf("and is greater than 5\n", str, n);
 	}
 	else if (n == 0)
 	{
-	printf("%s %i is and 0\n", str, n);
+	printf("and is 0\n", str, n);
 	}
 	else if (n < 6 && n != 6)
 	{
-	printf("%s %i is and is less than 6 and not 0\n", str, n);
+	printf("and is less than 6 and not 0\n", str, n);
 	}
 
 	return (0);
