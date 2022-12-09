0cad88bcb5826de49d88f2f4443d011ef9dd159e
dev0nat <natdev01@tuta.io>
2022-12-09 13:54:46 -0800

    fixes

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *  main - prints 0 - 9 with ,
+ *
+ *  Return: returns 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+	putchar(x);
+
+	if (x != '9')
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
