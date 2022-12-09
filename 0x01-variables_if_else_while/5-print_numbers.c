dc3fb92505ffd4ebcd239f30a0d2f73574109786
dev0nat <natdev01@tuta.io>
2022-12-09 12:31:07 -0800

    print out 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *  main - prints out 0-9
+ *
+ *  Return: returns 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	for (; x < 10; x++)
+	puts(x)
+	putchar('\n');
+	return (0);
+}
+
+
