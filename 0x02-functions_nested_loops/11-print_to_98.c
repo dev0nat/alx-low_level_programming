ac311f1faefdbe487469ca9ce5d584318ba55b78
dev0nat <natdev01@tuta.io>
2022-12-13 11:03:07 -0800

    naturalNumbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  print_to_98 - function prints natural number upto 98
+ *
+ * @n : holds the input
+ *
+ *  Return: returns the output
+ */
+void print_to_98(int n)
+{
+
+	for ( ; n <= 98; n++)
+		_putchar(n);
+	_putchar(',');
+	_putchar(' ');
+
+}
