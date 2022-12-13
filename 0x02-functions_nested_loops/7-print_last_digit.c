bcc11f739bb186e5c2a5cf7c63c20c4a0c7019b1
dev0nat <natdev01@tuta.io>
2022-12-13 10:00:22 -0800

    lastDigit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  print_last_digit - takes and input and gives input %
+ *
+ * @n : holds the input
+ *
+ *  Return: returns the lasdigit of our input
+ */
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
