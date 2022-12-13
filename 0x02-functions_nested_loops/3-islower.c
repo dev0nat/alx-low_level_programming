35fdabc992d2ecc78933616253ad693a8a6bf4ef
dev0nat <natdev01@tuta.io>
2022-12-13 06:59:59 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  _islower - function checkes whether an input is lowercase or not
+ *
+ * @c: checks input of function
+ *
+ *  Return: returns an integer c (Success)
+ */
+int _islower(int c)
+{
+
+	c = 'c';
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
