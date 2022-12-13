614b79f92d992c18641d36b01ba93c711cac2585
dev0nat <natdev01@tuta.io>
2022-12-13 03:59:04 -0800

    identation and bracket fixes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,12 +3,16 @@
 void print_alphabet_x10(void)
 {
 
-	int i,x10;
+	int i, x10;
 
 	for (x10 = 0; x10 < 10; x10++)
+	{
 		for (i = 'a'; i <= 'z'; i++)
+		{
 			_putchar(i);
+		}
 	_putchar('\n');
+	}
 
 
 }
