de2d43f9ae04192f7ecc6343a7c33030f0fdf460
dev0nat <natdev01@tuta.io>
2022-12-13 10:42:53 -0800

    sumOfintegers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *  add - functions takes two integers and prints out their sum
+ *
+ * @a & @b holds inputs
+ *
+ *  Return: returns sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
