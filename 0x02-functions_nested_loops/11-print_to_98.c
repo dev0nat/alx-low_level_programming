adc3228b6e67e3b5bfda9d49b36a803b3ca0c7da
dev0nat <natdev01@tuta.io>
2022-12-13 11:33:34 -0800

    fixed quotation

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 {
 	if (n < '98')
 	{
-		for (n = n; n <= '98'; n++)
+		for (n = n; n <= '98; n++)
 		{
 			_putchar(n);
 		_putchar(',');
@@ -20,7 +20,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (n = n; n > '98'; n--)
+		for (n = n; n > 98; n--)
 		{
 			_putchar(n);
 		_putchar(',');
