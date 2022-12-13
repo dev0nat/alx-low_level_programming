de277293297be923202af1316a75d684fe2e9254
dev0nat <natdev01@tuta.io>
2022-12-13 11:42:56 -0800

    switched to printf

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,9 +13,10 @@ void print_to_98(int n)
 	{
 		for (n = n; n <= 98; n++)
 		{
-			_putchar(n);
-		_putchar(',');
-		_putchar(' ');
+			/*_putchar(n);
+			_putchar(',');
+			_putchar(' ');*/
+			printf("%d, ",n);
 		}
 	}
 	else
