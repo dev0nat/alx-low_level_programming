caebda16b483201733df8cdffcd8f1ee592e3ca0
dev0nat <natdev01@tuta.io>
2022-12-13 11:29:10 -0800

    identation

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,12 +9,24 @@
  */
 void print_to_98(int n)
 {
-
-	for ( ; n <= 98; n++)
+	if (n < '98')
 	{
-		_putchar(n);
-	_putchar(',');
-	_putchar(' ');
+		for (n = n; n <= '98'; n++)
+		{
+			_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+		}
 	}
+	else
+	{
+		for (n = n; n > '98'; n--)
+		{
+			_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+		}
+	}
+
 
 }
