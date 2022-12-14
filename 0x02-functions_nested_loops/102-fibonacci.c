2098ff92781381e9a33c5c348bf898bdc42b31c9
dev0nat <natdev01@tuta.io>
2022-12-14 02:56:12 -0800

    advanced fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - prints out first 50 Fibonacci numbers starting with 1 and 2.
+ *
+ * Return: returns 0 (Success)
+ */
+int main(void)
+{
+	unsigned long f1 = 0, f2 = 1, sum;
+
+	for (int i = 0; i < 50; i++)
+	{
+		sum = f1 + f2;
+		printf("%lu", sum);
+
+		f1 = f2;
+		f2 = sum;
+
+		if (i == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
