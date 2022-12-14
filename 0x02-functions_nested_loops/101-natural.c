37e4df0b06e8b941182d951b19ebfde164ef12f5
dev0nat <natdev01@tuta.io>
2022-12-14 01:58:36 -0800

    sumOfNaturalNumbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+int main(void)
+{
+	int n;
+	int div;
+
+	for(n = 0;n < 1024; n++)
+	if (n % 15 == 0)
+	{
+		div = div + n;
+	}
+	printf("The sum is: %d \n",div);
+
+
+	return(0);
+}
+
+
+
+
+
+	return (0);
+}
+
+
