bd9302587c2cc54947c423abb2b29ba7f134f0b8
dev0nat <natdev01@tuta.io>
2022-12-14 02:16:25 -0800

    sumOf

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *  main - function prints sumofNatural numbers under 1024 divisable by 3 and 5
+ *
+ *  Return: returns 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int div;
+
+	for (n = 0; n < 1024; n++)
+	if (n % 15 == 0)
+	{
+		div = div + n;
+	}
+	printf("%d \n", div);
+
+	return (0);
+}
