dd11ebc573afadf16321769cf6c6beece2eb2e70
dev0nat <natdev01@tuta.io>
2022-12-15 13:26:20 -0800

    printLine

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *  print_line - function prints an n number of dash
+ *
+ * @n: holds our input
+ * @i: for iteration
+ *
+ *  Return: returns nothing
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		_putchar('\n');
+		}
+	}
+}
