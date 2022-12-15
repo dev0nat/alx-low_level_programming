a1cd7a78c7f22528da1fee73d484a3bf94a83122
dev0nat <natdev01@tuta.io>
2022-12-15 12:33:23 -0800

    multiplies

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  mul - function multiplies two integers
+ *
+ * @a: holds an integer
+ * @b: holds another integer
+ *
+ *  Return: two value sum
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
