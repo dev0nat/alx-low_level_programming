2b28774f9b84bd764031095cfcbc09bfc8621504
dev0nat <natdev01@tuta.io>
2022-12-15 14:17:13 -0800

    main function description and betty style

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *  main - function prints some conditions.
+ *
+ *
+ *  Return: returns 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+
+	return (0);
+
+}
