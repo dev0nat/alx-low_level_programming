02cc8506bd3358f5b935f5643801c521b69ec665
dev0nat <natdev01@tuta.io>
2022-12-15 12:41:40 -0800

    numPrinter

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  print_numbers - function prints from 0 - 9
+ *
+ *
+ *  Return: returns nothing
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		_putchar(i);
+	_putchar('\n');
+}
