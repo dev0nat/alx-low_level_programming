ad3fbc6b74cce1e6560503f034294e0e9fdcd8b7
dev0nat <natdev01@tuta.io>
2022-12-16 10:11:11 -0800

    primeFactors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: return 0 on completion
+ */
+int main(void)
+{
+	int inc,pf;
+	long int num = 612852475143;
+
+	for (inc = 2; num > 1; inc++)
+	{
+		while (num % inc == 0)
+		{
+			num = num / inc;
+			pf = inc;
+		}
+	}
+
+	printf("%d\n", pf);
+	return (0);
+}
