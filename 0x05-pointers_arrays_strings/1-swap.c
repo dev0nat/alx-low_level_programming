c92ef094009f4f83284b3e73edea0fa313d1b869
dev0nat <natdev01@tuta.io>
2022-12-19 10:13:18 -0800

    swapIntegers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps integer values with each other
+ *
+ * @a: pointer int
+ * @b: pointer int
+ *
+ * Return: returns nothing.
+ */
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	*c = *a;
+	*b = *c;
+	*a = *b;
+
+}
