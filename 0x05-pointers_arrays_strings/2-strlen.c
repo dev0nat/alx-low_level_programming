fe6d0aa3107e70b16b667751db925362fa979819
dev0nat <natdev01@tuta.io>
2022-12-19 10:36:27 -0800

    added a strlen similar impelementation and removed string.h header

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
+/**
+ * _strlen - function prints length of a string
+ *
+ * @s: holds our pointer char
+ *
+ * return: returns the length of *s
+ */
 int _strlen(char *s)
 {
-	strlen(*s);
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+
+	}
+	return (length);
 }
