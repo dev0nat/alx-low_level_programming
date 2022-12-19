28fd68535e6ca85e15e79e113549ef0bf199d300
dev0nat <natdev01@tuta.io>
2022-12-19 11:58:48 -0800

    fixed identation

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,9 @@ void puts2(char *str)
 {
 	int i;
 
-        for (i = 0; str[i] != '\0'; i += 2)
-        {
-                _putchar(str[i]);
-        }
-        _putchar('\n');
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
