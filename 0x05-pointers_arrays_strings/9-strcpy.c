6e43aed94c42cad1595ad353f2595da985662169
dev0nat <natdev01@tuta.io>
2022-12-19 13:03:38 -0800

    stringCopy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string from src to dest
+ * @dest: destination pointer
+ * @src: source pointer
+ * Return: destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+
+}
