61bacc3d121eff8a179aabcb845841a2aa727edb
dev0nat <natdev01@tuta.io>
2022-12-19 11:33:31 -0800

    reverseString

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - reverses our input string
+ * @s: holds our input
+ * Return: string reversed
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
