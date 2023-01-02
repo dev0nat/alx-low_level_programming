118c9157ecdc663f27c96b71fe54432ddd2a7ccb
dev0nat <natdev01@tuta.io>
2023-01-02 10:48:07 -0800

    added function description

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _memcpy - function copies n number of bytes from src to dest
+ *
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes to be copied
+ * Return: returns s
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
