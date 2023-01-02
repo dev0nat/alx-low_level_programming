6cc9b4efb4bce3fe93b6b722a8f64326558e8173
dev0nat <natdev01@tuta.io>
2023-01-02 11:21:05 -0800

    locateChar

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - function locates a character in a string
+ *
+ * @s: area to be filled
+ * @c: constant byte
+ * Return: returns s or null depending on the condition
+ */
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index)
+	}
+	return ('\0');
+}
