d152c774680cd5d18437da198bc3fd55c2f22d9e
dev0nat <natdev01@tuta.io>
2023-01-16 10:08:20 -0800

    added struct discription

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog type with the following elements
+ *
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
 struct dog
 {
 	char *name;
