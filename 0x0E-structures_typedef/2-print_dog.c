5ab31deadbdf6a1c3750c66f9ea827733b8dc59b
dev0nat <natdev01@tuta.io>
2023-01-16 12:06:05 -0800

    fixed logic

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,9 +10,12 @@ void print_dog(struct dog *d)
 {
 
 	if (d == NULL)
-		printf("(nil)");
-	else if (d->name == NULL)
-		printf("Name: (nil)");
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	if (d->owner == NULL)
+		d->owner = "(nil)\n";
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
