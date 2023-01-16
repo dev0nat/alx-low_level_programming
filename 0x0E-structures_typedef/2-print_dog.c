3583bf92a908b36a2c82d3bf10795896377801c2
dev0nat <natdev01@tuta.io>
2023-01-16 12:36:55 -0800

    fixed output error

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,13 +9,24 @@
 void print_dog(struct dog *d)
 {
 
-	if (d == NULL)
-		return;
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			 printf("Name: %s\n", d->name);
 
-	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	if (d->owner == NULL)
-		d->owner = "(nil)\n";
+		/**if (d->age == NULL)
+		*	printf = "Age: (nil)\n";
+		*else
+		*/
+		printf("Age: %f\n", d->age);
 
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			 printf("Owner: %s\n", d->owner);
+
+		/* printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner); */
+	}
 }
