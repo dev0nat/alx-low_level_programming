f0dce06fc53d3cf0f7f7758d7598130be47e57a1
dev0nat <natdev01@tuta.io>
2023-01-18 08:50:31 -0800

    intIndex

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function searches for an integer
+ * 
+ * @array: array to be searched
+ * @size: array size
+ * @cmp: pointer to the comparing function
+ * Return: first index for which cmp doesn't return 0 to or -1.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array || !cmp)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
