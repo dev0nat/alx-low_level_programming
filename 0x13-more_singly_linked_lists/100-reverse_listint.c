8093057cbcb3145114e6c91795240ad25ccbc375
dev0nat <natdev01@tuta.io>
2023-01-31 10:54:41 -0800

    reverseList

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a listint_t linked list.
+ *
+ * @head: head of the linked list
+ * Return: returns pointer to the first node of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *p, *n;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	p = NULL;
+
+	while ((*head)->next != NULL)
+	{
+		n = (*head)->next;
+		(*head)->next = p;
+		p = *head;
+		*head = n;
+	}
+
+	*head = p;
+
+	return (*head);
+}
