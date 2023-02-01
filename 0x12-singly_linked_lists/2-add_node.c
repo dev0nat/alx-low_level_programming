0eebe9da57ea7aed029f47bdd2a3b075cd01505a
dev0nat <natdev01@tuta.io>
2023-02-01 00:26:14 -0800

    Null check and strlen function for str

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,12 +11,13 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
-	if (head == NULL)
+	if (head && str == NULL)
 		return (0);
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (0);
 	new->str = strdup(str);
+	new->len = strlen(str);
 	new->next = *head;
 	*head = new;
 	return (new);
